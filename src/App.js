f4304642fe97e8fc9608645b68d9091d64ce24c9
Ali Atashinbar <ali.atashinbar@gmail.com>
2023-03-15 20:30:32 +0330

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg'
 import banner from './images/naseri.jpg'
 import './App.css'
 
 function App() {
 	return (
 		<div className='App'>
+			<div className='banner'>
+				<a href='https://us06web.zoom.us/j/81718895832?pwd=ZlRRMXBJYVFQRFY5WWJVa0E2UW5Sdz09'>
+					برای مشاهده آنلاین مراسم کلیک کنید
+				</a>
+				<strong>داشتن اکانت وب سایت zoom الزامی است</strong>
+			</div>
 			<img src={banner} alt='logo' />
 		</div>
 	)
